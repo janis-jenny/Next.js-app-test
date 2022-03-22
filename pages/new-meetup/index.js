98fbc074b744a947b6b9469edbb8044922856acf
Jenny Cárdenas <pcardenasvm@gmail.com>
2022-03-21 19:31:37 -0500

    Add navigation programatically to home with useRouter

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,9 @@
 import NewMeetupForm from "../../components/meetups/NewMeetUpForm";
-
+import { useRouter } from 'next/router';
 // my-domain.com/new-meetup
 
 function NewMeetUpPage() {
+    const router = useRouter();
     async function addMeetUpHandler(enteredData) {
         // insert the url to we want to send a request, here is the internal api that is hosted 
         //by the same server as being use for serving the page, this will trigger the handler from the api w this path
@@ -13,9 +14,9 @@ function NewMeetUpPage() {
             headers: { 'Content-Type': 'application/json'}
         });
         const data = response.json();
-        console.log(data)
+        console.log(data);
+        router.push('/') //replace is for not get back w the back btn
     }
-
     return <NewMeetupForm onAddMeetup={addMeetUpHandler} /> 
     // No () here cause it is gonna be execurted in props function at NewMeetupForm when user submit the form
 }
