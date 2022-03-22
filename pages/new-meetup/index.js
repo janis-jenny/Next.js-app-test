70f1cf5f52471170b6050261a5eb86918579d66e
Jenny Cárdenas <pcardenasvm@gmail.com>
2022-03-21 19:08:19 -0500

    Fix api route to create a meetup in databse from the browser form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -3,8 +3,17 @@ import NewMeetupForm from "../../components/meetups/NewMeetUpForm";
 // my-domain.com/new-meetup
 
 function NewMeetUpPage() {
-    function addMeetUpHandler(enteredData) {
-        console.log(enteredData)
+    async function addMeetUpHandler(enteredData) {
+        // insert the url to we want to send a request, here is the internal api that is hosted 
+        //by the same server as being use for serving the page, this will trigger the handler from the api w this path
+        // send a POST request w the 2nd argument
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredData), //this is a js object with the data we want to store in the db
+            headers: { 'Content-Type': 'application/json'}
+        });
+        const data = response.json();
+        console.log(data)
     }
 
     return <NewMeetupForm onAddMeetup={addMeetUpHandler} /> 
